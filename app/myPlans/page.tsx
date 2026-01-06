73880f4c90ad2818efcfc2a6495592676995265e
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2026-01-05 18:42:15 -0700

    Initial routes

---
+++ app/myPlans/page.tsx
@@ -0,0 +1,9 @@
+import {Fragment} from "react";
+
+export default function MyPlansPage() {
+    return (
+        <Fragment>
+            <h1>My Plans Page</h1>
+        </Fragment>
+    )
+}
---
+++ app/planDetails/page.tsx
@@ -0,0 +1,9 @@
+import {Fragment} from "react";
+
+export default function PlanDetailsPage() {
+    return (
+        <Fragment>
+            <h1>Plan Details Page</h1>
+        </Fragment>
+    )
+}
